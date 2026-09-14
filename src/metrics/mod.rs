b0203832d86fb2ad1f58504de07d6c8207a154f6
Steve <tarkasteve@gmail.com>
2026-09-14 12:32:24 +1000

    maint: Convert metrics initialisation to be more data-driven.

--- src/metrics/mod.rs
+++ src/metrics/mod.rs
@@ -27,6 +27,25 @@ pub const METRIC_HTTP_REQUESTS_TOTAL: &str = "vicarian_http_requests_total";
 pub const METRIC_METRICS_SCRAPE_TOTAL: &str = "vicarian_metrics_scrape_total";
 pub const METRIC_TLS_REQUESTS_TOTAL: &str = "vicarian_tls_requests_total";
 
+
+enum MetricType {
+    Counter(&'static str, &'static str),
+    Gauge(&'static str, &'static str),
+}
+
+// Counters and their descriptions, described at startup.
+const METRICS_LIST: &[MetricType] = &[
+    MetricType::Counter(METRIC_ACME_HTTP01_ENDPOINT_TOTAL, "Total number of ACME HTTP-01 challenge endpoint requests"),
+    MetricType::Counter(METRIC_ACME_HTTP01_NOTFOUND_TOTAL, "Total number of ACME HTTP-01 challenge not found responses"),
+    MetricType::Counter(METRIC_AUTH_INVALID_TOTAL, "Total number of invalid authentication attempts"),
+    MetricType::Counter(METRIC_AUTH_VALID_TOTAL, "Total number of valid authentication attempts"),
+    MetricType::Counter(METRIC_HTTP_REDIRECTS_TOTAL, "Total number of HTTP redirects served"),
+    MetricType::Counter(METRIC_HTTP_REQUESTS_TOTAL, "Total number of HTTP requests received"),
+    MetricType::Counter(METRIC_METRICS_SCRAPE_TOTAL, "Total number of metrics endpoint scrapes"),
+    MetricType::Counter(METRIC_TLS_REQUESTS_TOTAL, "Total number of TLS handshake requests"),
+    MetricType::Gauge(METRIC_ACME_NEXT_RENEWAL_TIMESTAMP_SECS, "Unix timestamp of the next ACME certificate renewal"),
+];
+
 static METRICS: OnceLock<Metrics> = OnceLock::new();
 
 #[derive(Debug)]
@@ -58,59 +77,18 @@ impl Metrics {
     }
 
     fn describe_metrics(&self) {
-        counter!(METRIC_ACME_HTTP01_ENDPOINT_TOTAL).absolute(0);
-        describe_counter!(
-            METRIC_ACME_HTTP01_ENDPOINT_TOTAL,
-            "Total number of ACME HTTP-01 challenge endpoint requests"
-        );
-
-        counter!(METRIC_ACME_HTTP01_NOTFOUND_TOTAL).absolute(0);
-        describe_counter!(
-            METRIC_ACME_HTTP01_NOTFOUND_TOTAL,
-            "Total number of ACME HTTP-01 challenge not found responses"
-        );
-
-        counter!(METRIC_AUTH_INVALID_TOTAL).absolute(0);
-        describe_counter!(
-            METRIC_AUTH_INVALID_TOTAL,
-            "Total number of invalid authentication attempts"
-        );
-
-        counter!(METRIC_AUTH_VALID_TOTAL).absolute(0);
-        describe_counter!(
-            METRIC_AUTH_VALID_TOTAL,
-            "Total number of valid authentication attempts"
-        );
-
-        counter!(METRIC_HTTP_REDIRECTS_TOTAL).absolute(0);
-        describe_counter!(
-            METRIC_HTTP_REDIRECTS_TOTAL,
-            "Total number of HTTP redirects served"
-        );
-
-        counter!(METRIC_HTTP_REQUESTS_TOTAL).absolute(0);
-        describe_counter!(
-            METRIC_HTTP_REQUESTS_TOTAL,
-            "Total number of HTTP requests received"
-        );
-
-        counter!(METRIC_METRICS_SCRAPE_TOTAL).absolute(0);
-        describe_counter!(
-            METRIC_METRICS_SCRAPE_TOTAL,
-            "Total number of metrics endpoint scrapes"
-        );
-
-        counter!(METRIC_TLS_REQUESTS_TOTAL).absolute(0);
-        describe_counter!(
-            METRIC_TLS_REQUESTS_TOTAL,
-            "Total number of TLS handshake requests"
-        );
-
-        gauge!(METRIC_ACME_NEXT_RENEWAL_TIMESTAMP_SECS).set(0);
-        describe_gauge!(
-            METRIC_ACME_NEXT_RENEWAL_TIMESTAMP_SECS,
-            "Unix timestamp of the next ACME certificate renewal"
-        );
+        for m in METRICS_LIST {
+            match m {
+                MetricType::Counter(name, desc) => {
+                    counter!(*name).absolute(0);
+                    describe_counter!(*name, *desc);
+                }
+                MetricType::Gauge(name, desc) => {
+                    gauge!(*name).set(0);
+                    describe_gauge!(*name, *desc);
+                }
+            }
+        }
     }
 
     pub async fn run(&self) {
